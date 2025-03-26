52ef3eaa7396ed53db634346ef36f248f18da2eb
thaytuh <htaylorkrass@gmail.com>
2025-03-26 11:41:26 -0700

    added text to html func in textnode and added edge case testing in test_textnode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -12,6 +12,62 @@ class TestTextNode(unittest.TestCase):
         node4 = TextNode("This is a URL text node", TextType.LINK)
         self.assertEqual(node, node2)
         
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+        
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+        
+    def test_italic(self):
+        node = TextNode("This is italic", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is italic")
+        
+    def test_code(self):
+        node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is code")
+        
+    def test_link(self):
+        node = TextNode("Click here", TextType.LINK, url="https://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click here")
+        self.assertEqual(html_node.props["href"], "https://example.com")
+        
+    def test_image(self):
+        node = TextNode("Alt text here", TextType.IMAGE, url="https://example.com/image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props["src"], "https://example.com/image.png")
+        self.assertEqual(html_node.props["alt"], "Alt text here")
+        
+    def test_invalid_type(self):
+        with self.assertRaises(Exception) as context:
+            node = TextNode("Unknown type", None)  # Passing an invalid or `None` type
+            text_node_to_html_node(node)
+        self.assertEqual(str(context.exception), "invalid text type")
+        
+    def test_link_with_missing_url(self):
+        node = TextNode("Click here", TextType.LINK, url=None)
+        with self.assertRaises(Exception) as context:
+            text_node_to_html_node(node)
+        self.assertEqual(str(context.exception), "Link node must have a valid URL")
+        
+    def test_image_with_missing_url(self):
+        node = TextNode("Image Alt Text", TextType.IMAGE, url=None)
+        with self.assertRaises(Exception) as context:
+            text_node_to_html_node(node)
+        self.assertEqual(str(context.exception), "Image node must have a valid URL")
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -26,4 +27,25 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-    
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            if not text_node.url:
+                raise Exception("Link node must have a valid URL")
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            if not text_node.url:
+                raise Exception("Image node must have a valid URL")
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("invalid text type")
