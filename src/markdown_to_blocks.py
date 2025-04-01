bb027f57bca8fcee253951e00103b59742db93f7
thaytuh <htaylorkrass@gmail.com>
2025-04-01 11:24:38 -0700

    removed unused empty list

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,7 +1,6 @@
 def markdown_to_blocks(markdown):
     final_list = []
     split_blocks = markdown.split("\n\n")
-    working_list = []
     for block in split_blocks:
         stripped_block = block.strip()
         if stripped_block:
