cb4d3f394bc4c11efa171f072e347c569813d6d7
thaytuh <htaylorkrass@gmail.com>
2025-04-01 14:27:34 -0700

    added actual page generation functionality, need to tweak and finalize

---
+++ src/generate_page.py
@@ -0,0 +1,64 @@
+import os, shutil
+from pathlib import Path
+from markdown_to_html_node import markdown_to_html_node
+from htmlnode import HTMLNode
+from extract_title import extract_title
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
+    markdown_file = get_file_from_source_path(from_path)
+    template_file = get_file_from_source_path(template_path)
+    markdown_as_html = markdown_to_html_node(markdown_file).to_html()
+    title = extract_title(markdown_file)
+    template_file.replace("{{ Title }}", title)
+    template_file.replace("{{ Content }}", markdown_as_html)
+    create_directory(dest_path)
+    create_html_file(dest_path, markdown_as_html)
+
+def has_file_type(directory, extension):
+    file_list = []
+    path = Path(directory)
+    md_files = path.glob(extension)
+    for file in md_files:
+        file_list.append(file)
+    return file_list
+
+def get_file_from_source_path(from_path):
+    try:
+        with open(from_path, 'r', encoding='utf-8') as file:
+            file_content = file.read()
+        return file_content
+    except FileNotFoundError:
+        print(f"Error: File not found")
+        return None
+    except Exception as e:
+        print(f"An error occured: {e}")
+        return None
+
+
+def create_directory(destination_path):
+    dir_name = os.path.dirname(destination_path)
+    os.makedirs(dir_name, exist_ok=True)
+
+def create_html_file(destination_path, content):
+    try:
+        with open(destination_path, 'w') as html_file:
+            html_file.write(content)
+        return True
+    except Exception as e:
+        print(f"Error creating HTML file: {e}")
+        return False
+
+
+
+def clear_directory(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f"Failed to delete {file_path}. Reason: {e}")
+
--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from textnode import TextNode, TextType
 from source_to_dest import copy_dir
+from generate_page import generate_page, clear_directory
 
 def main():
+    clear_directory("./public")
     copy_dir("./static", "./public")
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 
 main()
